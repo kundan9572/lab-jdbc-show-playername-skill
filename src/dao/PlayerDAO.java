03100acf226109ceb9a4d8966f35ea77ffc79117
kundan kumar <kd.kundan9572@gmail.com>
2020-09-11 12:19:40 +0530

    Initial Comit :

--- src/dao/PlayerDAO.java
+++ src/dao/PlayerDAO.java
@@ -1,1 +1,46 @@
+package dao;
 
+import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Player;
+import model.Skill;
+import utility.ConnectionManager;
+
+public class PlayerDAO {
+	
+	public List<Player> getAllPlayer() throws ClassNotFoundException, SQLException, IOException{
+		
+		List<Player> playerList = new ArrayList<Player>();
+		
+		Player player= null;
+		
+		ConnectionManager con =new ConnectionManager();
+		
+		Statement st= con.getConnection().createStatement();
+		
+		String sql=" SELECT * FROM PLAYER";
+		ResultSet rs = st.executeQuery(sql);
+		
+		while(rs.next())
+		{
+			player= new Player();
+			Long pid= rs.getLong("ID");
+			player.setPlayerId(pid);
+			String name =rs.getString("NAME");
+			player.setName(name);
+			String country = rs.getString("COUNTRY");
+			player.setCountry(country);
+			Long skillid = rs.getLong("SKILLID");
+			player.setSkill(skillid);
+			playerlist.add(player);
+		}
+	}
+	
+	return playerlist;
+	
+}
--- src/dao/SkillDAO.java
+++ src/dao/SkillDAO.java
@@ -1,1 +1,47 @@
+package dao;
 
+import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Player;
+import model.Skill;
+import utility.ConnectionManager;
+
+public class SkillDAO{
+	
+	public Skill getSkillID(Long ID) throws ClassNotFoundException, SQLException, IOException {
+		
+		
+		
+		
+		List<Skill> skillList = new ArrayList<Skill>();
+		
+		Skill skill= null;
+		
+		ConnectionManager con =new ConnectionManager();
+		
+		Statement st= con.getConnection().createStatement();
+		
+		String sql=" SELECT * FROM SKILL";
+		ResultSet rs = st.executeQuery(sql);
+		
+		
+		
+		while(rs.next())
+		{
+			skill = new Skill();
+			Long skillid = rs.getLong("SKILLID");
+			skill.setSkillId(skillid);
+			String name = rs.getString("NAME");
+			skill.setSkillName(name);
+			skillList.add(skill);
+		}
+		
+		
+	}
+
+}
